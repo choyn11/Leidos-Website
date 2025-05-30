975ee46d6ba7ae2e62fca7b776c535ee1deec88a
choyn11 <nasyachoy06@gmail.com>
2025-05-30 16:56:26 -0500

    changed a couple lines

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const menu = document.querySelector('#Sun');
 const menuLinks = document.querySelector('.navigationContainer');
 
-menu.addEventListener('click', function() {
-  menuLinks.classList.toggle('active');
+menu.addEventListener('click', () => {
+  if (menuLinks.classList.contains('active')) {
+    menuLinks.classList.add('closing');
+    menuLinks.addEventListener('animationend', menuClose, {once: true})
+  } else {
+    menuLinks.classList.add('active');
+  }
 });
+
+function menuClose() {
+  menuLinks.classList.remove('active', 'closing');
+}
